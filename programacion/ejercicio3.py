1327f7c0681d48b9df6949cfff2093d133f355cc
camilospina11 <camilo110393@gmail.com>
2023-11-18 12:09:17 -0500

    examen practico

--- programacion/ejercicio3.py
+++ programacion/ejercicio3.py
@@ -2,16 +2,20 @@
 
 
 def verificar_primos(numero):
-    contador = 0
-    if numero < 1 :
-      return print("ingrese un numero mayor a 1")
-    for i in range(1, numero +1 ):
-     if (numero % i) == 0: 
-        contador+=1
-        
-    
+
+
+    if numero <= 1 :
+         print("por favor escribir un numero entero positivo mayor a uno :")
+    else :
+       contador = 0  
+    for  i in range(1 , numero+ 1):
+          if numero % (i *0.5) == 0 :
+              contador += 1      
+                
+          else:
+              print("no es primo")
         
 numero=int(input("ingrese un numero"))
 
 verificar_primos(numero)
-print("es un numero primo")
+print(f"{numero} es un numero primo")
